af6d29393c7060bb102062b9a001f785feb577f5
saikrishna488 <oltvez@gmail.com>
2024-09-10 12:33:06 +0530

    fixed bugs

--- bangaram_backend/server.js
+++ bangaram_backend/server.js
@@ -22,10 +22,6 @@ connectMongoDb();
 
 const corsOptions = {
     origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
-            'http://127.0.0.1:5000',
-            'http://localhost:5000',
             'https://bangaram-project.vercel.app',
             'https://bangaram-project-saikrishna488s-projects.vercel.app',
             'https://bangaram-project-git-main-saikrishna488s-projects.vercel.app',  
