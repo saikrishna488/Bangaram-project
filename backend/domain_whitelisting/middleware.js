9245eda7bf97a33cffd4febc297b9e2bca0d7e78
saikrishna488 <oltvez@gmail.com>
2024-09-25 21:48:08 +0530

    added dropgame

--- backend/domain_whitelisting/middleware.js
+++ backend/domain_whitelisting/middleware.js
@@ -1,6 +1,6 @@
 // domainWhitelistMiddleware.js
 
-const allowedDomains = ['bangaram-project-saikrishna488s-projects.vercel.app', 'bangaram-project.vercel.app','bangaram-project-git-main-saikrishna488s-projects.vercel.app','localhost'];
+const allowedDomains = ['bangaram-project-saikrishna488s-projects.vercel.app', 'bangaram-project-git-main-saikrishna488s-projects.vercel.app','localhost'];
 
 function domainWhitelistMiddleware(req, res, next) {
     const origin = req.headers.origin;
